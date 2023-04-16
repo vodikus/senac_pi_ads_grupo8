1a4359c0d80b01a4024bde9146c72d7d360beaf7
imp.master <vodikus>
2023-04-16 19:08:35 -0300

    Ajuste na classe de conexão, permitindo customizar os dados de acesso utilizando variáveis de ambiente.

--- backend/includes/Connection.php
+++ backend/includes/Connection.php
@@ -1,14 +1,18 @@
 <?php
+
 Class Connection {
-  private  $server = "mysql:host=localhost;dbname=clube_livros";
-  private  $user = "clube_livros";
-  private  $pass = "senha";
   private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
   protected $con;
   
   public function openConnection() {
     try {
-      $this->con = new PDO($this->server, $this->user,$this->pass,$this->options);
+      $dbHost = (getenv('DBHOST')) ? getenv('DBHOST') : 'localhost';
+      $dbName = (getenv('DBNAME')) ? getenv('DBNAME') : 'clube_livros';
+      $dbUser = (getenv('DBUSER')) ? getenv('DBUSER') : 'clube_livros';
+      $dbPass = (getenv('DBPASS')) ? getenv('DBPASS') : 'senha';
+      $server = "mysql:host=$dbHost;dbname=$dbName";
+
+      $this->con = new PDO($server, $dbUser,$dbPass,$this->options);
       return $this->con;
     } catch (PDOException $e) {
       echo "Erro ao conectar com o banco de dados: " . $e->getMessage();
