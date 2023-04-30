20ca49d32b0eb88464ddb08ccef5bb2bc926dae8
Ivan Pereira <6559520+vodikus@users.noreply.github.com>
2023-04-29 23:48:42 -0300

    Revert "Delete api.php"
    This reverts commit 11e0626a51e90541cd93149ed99f5e8ecfc0f099.

---
+++ backend/api.php
@@ -0,0 +1,27 @@
+<?php
+define( 'INCLUDE_DIR', dirname( __FILE__ ) . '/controllers/' );
+
+// Regras de roteamento
+$rules = array( 
+    'AuthController'      => "auth/?(?'acao'[\w\-]*)\/?(?'param1'[\w\s]*)",
+    'UsuarioController'      => "usuarios/?(?'acao'[\w\-]*)\/?(?'param1'[\w\s]*)",
+    'EnderecoController'      => "enderecos/?(?'acao'[\w\-]*)\/?(?'param1'[\w\s]*)"
+);
+
+
+$uri = rtrim( dirname($_SERVER["SCRIPT_NAME"]), '/' );
+$uri = '/' . trim( str_replace( $uri, '', $_SERVER['REQUEST_URI'] ), '/' );
+$uri = urldecode( $uri );
+
+foreach ( $rules as $controller => $rule ) {
+    if ( preg_match( '~^/api/'.$rule.'$~i', $uri, $params ) ) {
+        include( INCLUDE_DIR . $controller . '.php' );
+        $objeto = new $controller();
+        $objeto->processarRequisicao( $_SERVER["REQUEST_METHOD"], $params);
+
+        exit();
+    }
+}
+
+// Nenhuma regra foi encontrada.
+include( dirname( __FILE__ ) . '/404.php' );
